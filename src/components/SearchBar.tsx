91575a925466b43cb6623eb270852c128db58ced
Adrian Sanchez <adrsanrod2@gmail.com>
2024-09-28 21:59:15 +0100

    adding card

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -73,7 +73,7 @@ function SearchBar() {
 
   //Fetch artist related info
   const fetchArtistInfo = (value) => {
-    
+
     const queryParameters = {
       method: 'GET',
       headers: {
@@ -146,6 +146,18 @@ function SearchBar() {
           }
         </Row>
       </Container>
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </>
   )
 }
